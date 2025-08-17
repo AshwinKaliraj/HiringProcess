e39e38df8efd95a2d948b644554b5a17c5ed65d4
AshwinKaliraj <ashwin.kaliraj@psiog.com>
2025-08-18 01:13:50 +0530

    3

--- hiring.cpp
+++ hiring.cpp
@@ -10,4 +10,35 @@ void hiringProcess() {
     getline(cin, jobDesc);
 
     cout << "Send crew request to HR\n";
+    cout << "Is it a regular hiring process? (y/n): ";
+    cin >> choice;
+
+    if (choice == 'n') {
+        cout << "Special hiring process\n";
+        return;
+    }
+
+    cout << "Post job internally\n";
+    cout << "Is there a suitable candidate? (y/n): ";
+    cin >> choice;
+
+    if (choice == 'n') {
+        cout << "Review existing resumes on file\n";
+        cout << "Is there a suitable candidate? (y/n): ";
+        cin >> choice;
+
+        if (choice == 'n') {
+            cout << "Develop recruitment plan\n";
+            cout << "Place advertisements\n";
+            cout << "Collect applications\n";
+            cout << "Qualified resume available? (y/n): ";
+            cin >> choice;
+
+            if (choice == 'n') {
+                cout << "Candidate not hired\n";
+                return;
+            }
+        }
+    }
+
    
