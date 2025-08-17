ab38a78284a26d35cd80c6f960b4690ab5f67d87
AshwinKaliraj <ashwin.kaliraj@psiog.com>
2025-08-18 01:04:47 +0530

    2

--- hiring.cpp
+++ hiring.cpp
@@ -2,3 +2,12 @@
 #include <string>
 using namespace std;
 
+void hiringProcess() {
+    string jobDesc, classification, candidate;
+    char choice;
+
+    cout << "Define job description: ";
+    getline(cin, jobDesc);
+
+    cout << "Send crew request to HR\n";
+   
